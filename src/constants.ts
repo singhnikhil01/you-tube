b6420802dd70f3343746bba0ceb50ed8f7f3edac
Singhsansar <singhsansar433@gmail.com>
2025-07-15 22:55:44 +0530

    changed the public URL default

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,4 @@
 export const DEFAULT_LIMIT = 5;
 export const RATELIMIT = 10;
-export const APP_URL = process.env.VERCEL_URL
+export const APP_URL =
+  process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000";
